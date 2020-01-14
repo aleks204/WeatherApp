20a458507f3f1a9a4872f8ee156336909925a54b
Alex Andros <abdomohamadoo9639@gmail.com>
2020-01-14 20:40:56 +0100

    new part

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 // api key : 4f9b61c1f110cb3e6c00bfc1646b8c95
+
+let latitude;
+let longitude;
+const notification = document.getElementsByClassName("notification")[0];
+
+function getLocation() {
+  if (navigator.geolocation);
+  navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}
+
+function onSuccess(position) {
+  console.log(position);
+}
+
+function onError(error) {
+  console.log("Unable to retrieve your location", error);
+  notification.innerHTML = error.message;
+
+  const p = document.createElement("p");
+  p.innerHTML = error.message;
+
+  notification.style.display = "block";
+  notification.appendChild(p);
+}
+
+getLocation();
